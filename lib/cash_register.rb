1687a302bc47a0bad1fb1dd38e42898327a67752
Yani Asari <yani.iman1@gmail.com>
2020-08-20 23:01:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+class CashRegister
+  
+  attr_accessor :discount, :total, :price, :items, :last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+  
+  def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
 
+    @cart << item_info
+
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    end
+    @total -= @total * @discount / 100
+    return "After the discount, the total comes to $#{@total}."
+  end 
+  
+  def items
+    item_names = []
+    @cart.each do | item_info |
+      for qty in 1..item_info[:quantity] 
+        item_names << item_info[:name]
+      end 
+  end 
+    item_names
+  end 
+  
+  def void_last_transaction
+    @total -= @price
+  end 
+  
+end 
+
+  
